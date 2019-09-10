82e8d3a9b0d97d2777a88efa414800045bdb4f2b
sara <51083712+SaraRasoulian@users.noreply.github.com>
2019-09-11 00:11:18 +0430

    Add files via upload

--- game.js
+++ game.js
@@ -52,6 +52,7 @@ startGame  =() =>{
 getNewQuestion=()=>{
     if(availableQuesions.length==0 || questionCounter>=MAX_QUESTIONS)
     {
+        localStorage.setItem("mostRecentScore",score);
         return window.location.assign("/end.html");
     }
 
