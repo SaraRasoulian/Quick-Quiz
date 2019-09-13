19c9dee75480a2b39089d5913db5d20d92230cde
sara <51083712+SaraRasoulian@users.noreply.github.com>
2019-09-14 00:16:48 +0430

    finished

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ const choices =Array.from( document.getElementsByClassName('choice-text'));
 const progressText =document.getElementById('progressText');
 const scoreText=document.getElementById('score');
 const progressBarFull=document.getElementById('progressBarFull');
+const loader =document.getElementById('loader');
+const game =document.getElementById('game');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -10,33 +12,37 @@ let score = 0;
 let questionCounter = 0;
 let availableQuesions = [];
 
-let questions = [
-  {
-    question: "Inside which HTML element do we put the JavaScript??",
-    choice1: "<script>",
-    choice2: "<javascript>",
-    choice3: "<js>",
-    choice4: "<scripting>",
-    answer: 1
-  },
-  {
-    question:
-      "What is the correct syntax for referring to an external script called 'xxx.js'?",
-    choice1: "<script href='xxx.js'>",
-    choice2: "<script name='xxx.js'>",
-    choice3: "<script src='xxx.js'>",
-    choice4: "<script file='xxx.js'>",
-    answer: 3
-  },
-  {
-    question: " How do you write 'Hello World' in an alert box?",
-    choice1: "msgBox('Hello World');",
-    choice2: "alertBox('Hello World');",
-    choice3: "msg('Hello World');",
-    choice4: "alert('Hello World');",
-    answer: 4
-  }
-];
+let questions = [];
+
+fetch("https://opentdb.com/api.php?amount=10&category=10&difficulty=easy&type=multiple")
+.then( res=>{
+  return res.json();
+})
+.then(loadedQuestions =>{
+  console.log(loadedQuestions.results);
+  questions =loadedQuestions.results.map(loadedQuestion=>{
+    const formattedQuestion ={
+      question: loadedQuestion.question
+    };
+
+    const answerChoices =[... loadedQuestion.incorrect_answers];
+    formattedQuestion.answer =Math.floor(Math.random()*3)+1;
+    answerChoices.splice(
+      formattedQuestion.answer - 1,
+      0,
+      loadedQuestion.correct_answer
+    );
+
+    answerChoices.forEach((choice, index) => {
+      formattedQuestion["choice" + (index + 1)] = choice;
+    });
+
+    return formattedQuestion;
+  });
+  startGame();
+}).catch(err =>{
+  console.error(err);
+});
 
 //CONSTANTS
 const CORRECT_BONUS = 10;
@@ -46,7 +52,9 @@ startGame  =() =>{
     questionCounter=0;
     score=0;
     availableQuesions=[...questions];
-    getNewQuestion();
+    getNewQuestion();    
+    game.classList.remove("hidden");
+    loader.classList.add('hidden');
 };
 
 getNewQuestion=()=>{
@@ -101,5 +109,3 @@ incrementScore=num=>{
   score+=num;
   scoreText.innerText=score;
 };
-
-startGame();
