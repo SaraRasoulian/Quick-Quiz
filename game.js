525a62b982cc029e2359c3b839575f4b9c709f0e
Sara Rasoulian <sara.rasolian@gmail.com>
2023-11-12 17:06:11 +0330

    relative links instead of absolute links

--- game.js
+++ game.js
@@ -61,7 +61,7 @@ getNewQuestion=()=>{
     if(availableQuesions.length==0 || questionCounter>=MAX_QUESTIONS)
     {
         localStorage.setItem("mostRecentScore",score);
-        return window.location.assign("/end.html");
+        return window.location.assign("end.html");
     }
 
     questionCounter++;
